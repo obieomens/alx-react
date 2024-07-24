d8ee85b251cf600b7fb4f1ef42bb361c126a0e99
obie <obieomens@gmail.com>
2024-07-24 18:37:42 +0100

    updatd

--- 0x01-react_intro/task_4/dashboard/src/App.js
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>School dashboard</h1>
       </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
     </div>
   );
 }
