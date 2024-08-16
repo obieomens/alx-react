4c3a5b589fc878882d3ca0336987fcd040a33b4e
obie <obieomens@gmail.com>
2024-08-16 21:03:32 +0100

    updated

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,5 @@
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
-function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject(object) {
+  return fromJS(object);
 }
-
-module.exports = getImmutableObject;
